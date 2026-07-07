dfcc1fd1a589508ad8c8c4692f30a76683c53f2e
Jonatas Gabriel <jonatasb2007@gmail.com>
2026-07-07 09:43:05 -0400

    Learning Jakarta annotations and setting up database properties

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,26 +1,57 @@
 package br.com.alura.screenmatch.model;
 
 import br.com.alura.screenmatch.service.ConsultaChatGPT;
+import jakarta.persistence.*;
 
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
     private String titulo;
+
     private Integer totalTemporadas;
+
     private Double avaliacao;
+
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
+
     private String atores;
+
     private String poster;
+
     private String sinopse;
 
     public Serie(DadosSerie dadosSerie){
+
         this.titulo = dadosSerie.titulo();
+
         this.totalTemporadas = dadosSerie.totalTemporadas();
+
         this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
+
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+
         this.atores = dadosSerie.atores();
+
         this.poster = dadosSerie.poster();
+
         this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
