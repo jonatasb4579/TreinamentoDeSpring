98f298c3fa2e9ef8c564b1a7a716189701553b27
Jonatas Gabriel <jonatasb2007@gmail.com>
2026-07-28 17:13:15 -0400

    adicionando info no repository

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -36,7 +36,8 @@ public class Principal {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar séries buscadas
-                                    
+                    4 - Buscar séries por Titulo               
+                    5 - Buscar séries por Ator
                     0 - Sair                                 
                     """;
 
@@ -54,6 +55,10 @@ public class Principal {
                 case 3:
                     listarSeriesBuscadas();
                     break;
+                case 4:
+                    buscarSeriePorTitulo();
+                case 5:
+
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -62,6 +67,8 @@ public class Principal {
             }
         }
     }
+
+
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
@@ -83,9 +90,9 @@ public class Principal {
         listarSeriesBuscadas();
         System.out.println("Escolha serie pelo nome: ");
         var nomeSerie = leitura.nextLine();
-        Optional<Serie> serie = series.stream()
-                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-                .findFirst();
+
+        Optional<Serie> serie =  repositorio.findByTituloContaningIgnorecase(nomeSerie);
+
         if (serie.isPresent()) {
 
             var serieEncontrada = serie.get();
@@ -116,5 +123,34 @@ public class Principal {
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
+    }
+
+    private void buscarSeriePorTitulo() {
+        System.out.println("Escolha serie pelo nome: ");
+        var nomeSerie = leitura.nextLine();
+        Optional<Serie> serieBuscada = repositorio.findByTituloContaningIgnorecase(nomeSerie);
+
+        if (serieBuscada.isPresent()){
+            System.out.println("Dados da Serie " + serieBuscada.get());
+
+        }else {
+            System.out.println("Serie não Encontrada");
+        }
+    }
+
+    private void buscarSeriesPorAtor(){
+        System.out.println("Qual nome para Busca");
+        var nomeAtor = leitura.nextLine();
+
+        System.out.println("Avaliações apartir de quanto: ");
+        var avaliacao = leitura.nextDouble();
+
+        List<Serie> seriesEncontradaas =
+                repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+
+        System.out.println("Nome das series que " + nomeAtor + "trabalhou: ");
+        seriesEncontradaas.forEach(s ->
+                System.out.println(s.getTitulo() +"avaliação: "+ s.getAvaliacao())
+        );
     }
 }
